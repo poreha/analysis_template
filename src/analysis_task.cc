193effd61f035e9e1ab57a9c6552d5ffb73ccbbb
mam-mih-val <mam.mih.val@gmail.com>
2020-09-25 13:57:00 +0300

    Excess fields are removed

--- src/analysis_task.cc
+++ src/analysis_task.cc
@@ -19,18 +19,10 @@ void AnalysisTask::Init(std::map<std::string, void *> &branch_map) {
 
   // linking necessary for analysis fields with enumerator for fast access to them
   fields_id_.insert(std::make_pair(FIELDS::HITS_TOF, event_header_config.GetFieldId("selected_tof_hits")));
-  fields_id_.insert(std::make_pair(FIELDS::CHARGE, mdc_vtx_tracks_config.GetFieldId("charge")));
-  fields_id_.insert(std::make_pair(FIELDS::TOF_DE_DX, meta_hits_config.GetFieldId("dEdx")));
-  fields_id_.insert(std::make_pair(FIELDS::MASS_2, meta_hits_config.GetFieldId("mass2")));
-  fields_id_.insert(std::make_pair(FIELDS::BETA, meta_hits_config.GetFieldId("beta")));
 
   // initializing histograms
   tof_multiplicity_distribution_ = new TH1F( "tof_multiplicity", ";TOF hits;counts", 100, 0, 100 );
   pT_distribution_ = new TH1F( "pT_distribution", ";p_{T}, [GeV/c];counts", 250, 0, 2.5 );
-  pT_vs_pseudorapidity_distribution_ = new TH2F( "pT_vs_pseudorapidity", ";#eta;p_{T}, [GeV/c]", 250, 0, 2.5, 250, 0.0, 2.5 );
-  pT_vs_mass2_distribution_ = new TH2F( "pT_vs_mass2", ";p_{T} / z, [GeV/c];m^{2}, [GeV^{2}/c^{4}]", 200, -2.0, 2.0, 200, 0.0, 10.0 );
-  p_vs_beta_distribution_ = new TH2F( "p_vs_beta", ";p / z, [GeV/c];#beta", 250, -1.5, 3.5, 240, 0.0, 1.2 );
-  p_vs_dEdx_tof_distribution_ = new TH2F( "p_vs_dEdx_tof", ";p / z, [GeV/c];dE/dx", 250, -1.5, 3.5, 200, 0.0, 15.0 );
 }
 
 void AnalysisTask::Exec() {
@@ -45,16 +37,8 @@ void AnalysisTask::Exec() {
 	auto pT = track.GetPt(); // getting transverse momentum
 	auto eta = track.GetEta(); // getting pseudorapidity
 	auto p = track.GetP(); // getting absolute value of momentum
-	auto charge = track.GetField<int>( fields_id_.at(FIELDS::CHARGE) ); // getting charge
-	auto mass2 = hit.GetField<float>( fields_id_.at(FIELDS::MASS_2) ); // getting squared mass
-	auto beta = hit.GetField<float>( fields_id_.at(FIELDS::BETA) ); // getting v/c
-	auto dEdx = hit.GetField<float>( fields_id_.at(FIELDS::TOF_DE_DX) ); // getting energy loss in TOF-system
 	// filling distributions
 	pT_distribution_->Fill(pT);
-	pT_vs_pseudorapidity_distribution_->Fill(eta, pT);
-	pT_vs_mass2_distribution_->Fill( pT/charge, mass2 );
-	p_vs_beta_distribution_->Fill( p/charge, beta );
-	p_vs_dEdx_tof_distribution_->Fill( p/charge, dEdx );
   }
 }
 
@@ -62,9 +46,5 @@ void AnalysisTask::Finish() {
   // Writing histograms to file
   tof_multiplicity_distribution_->Write();
   pT_distribution_->Write();
-  pT_vs_pseudorapidity_distribution_->Write();
-  pT_vs_mass2_distribution_->Write();
-  p_vs_beta_distribution_->Write();
-  p_vs_dEdx_tof_distribution_->Write();
 }
 } // namespace AnalysisTree
--- src/analysis_task.h
+++ src/analysis_task.h
@@ -26,10 +26,6 @@ public:
 private:
  enum class FIELDS{ // enumerator to fast access to detectors' fields
    HITS_TOF, 	// Hits in TOF-system
-   TOF_DE_DX, 	// energy loss in TOF-system
-   MASS_2, 		// mass2 measured with TOF-system
-   BETA, 		// v/c measured with TOF-system
-   CHARGE		// charge of particle
   };
   std::map<FIELDS, int> fields_id_; // map to match detectors' fields with enumerator
 
@@ -41,10 +37,6 @@ private:
 
   TH1F* tof_multiplicity_distribution_; 	// 1D histogram of sum multiplicity in TOF system
   TH1F* pT_distribution_; 					// 1D histogram of track transverse momentum distribution
-  TH2F* pT_vs_pseudorapidity_distribution_; 		// 2D histogram of transverse momentum vs rapidity
-  TH2F* pT_vs_mass2_distribution_; 			// 2D histogram of transverse momentum vs mass2 from TOF-system
-  TH2F* p_vs_beta_distribution_; 			// 2D histogram of momentum vs beta from TOF-system
-  TH2F* p_vs_dEdx_tof_distribution_; 		// 2D histogram of momentum vs energy loss in TOF-system
 };
 } // namespace AnalysisTree
 #endif // QUALITY_ASSURANCE_SRC_TREE_READER_H_
