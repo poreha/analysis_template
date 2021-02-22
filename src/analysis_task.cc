e4e2300cdfc066b51904971bb041e79df086fcc8
Alex <pavlikbulgakov@gmail.com>
2021-02-22 16:50:15 +0300

    ReReSetting
    again?

--- src/analysis_task.cc
+++ src/analysis_task.cc
@@ -24,13 +24,15 @@ void AnalysisTask::Init(std::map<std::string, void *> &branch_map) {
   fields_id_.insert(std::make_pair(FIELDS::BETA, meta_hits_config.GetFieldId("beta"))); //new edit looking for beta
   fields_id_.insert(std::make_pair(FIELDS::M2, meta_hits_config.GetFieldId("mass2")));
   // initializing histograms
+  
   tof_multiplicity_distribution_ = new TH1F( "tof_multiplicity", ";TOF hits;counts", 100, 0, 100 );
+  /*
   pT_distribution_ = new TH1F( "pT_distribution", ";p_{T}, [GeV/c];counts", 256, 0, 2.5 );
   phi_distribution_ = new TH1F("phi_distribution", ";phi; grad", 256, -4, 4);
   beta_distribution_ = new TH1F("beta", "beta; counts", 256, -1, 10);
   mass2_distribution_ = new TH1F("mass_squarred", "mass^2, [GeV]", 1024, -1, 13);
   p_distribution_ = new TH1F("p_distribution", "momentum, [GeV/c]", 1024, -50, 300);
-
+  */
   PTvRAPIDITY_ = new TH2F("Rapidity_vs_pT", ";Rapidity;p_{T}, GeV/c", 250, 0, 2, 250, 0, 1.8);
   PTvPSEUDORAPIDITY_ = new TH2F("Pseudo-Rapidity_vs_pT", ";Psudo-Rapidity;p_{T}, GeV/c", 250, 0, 2, 250, 0, 1.8);
   PHIvPT_ = new TH2F("Phi_vs_pT", ";Phi;p_{T}, GeV", 256, -3.5, 3.5, 250, 0, 5);
@@ -46,8 +48,9 @@ void AnalysisTask::Exec() {
   int n_tracks = mdc_vtx_tracks_->GetNumberOfChannels(); // number of tracks in current event
   for (int i = 0; i < n_tracks; ++i) { // loop over all tracks if current event
 	  auto track = mdc_vtx_tracks_->GetChannel(i); // getting track from track detector
-	  
-    int match_meta_hit = mdc_meta_matching_->GetMatchDirect(i); // getting index of matched with track TOF-system hit
+	  int pid = track.GetPid();
+
+    //int match_meta_hit = mdc_meta_matching_->GetMatchDirect(i); // getting index of matched with track TOF-system hit
     auto hit = meta_hits_->GetChannel(i); // getting matched with track hit in TOF-system
 	  auto charge = hit.GetField<int>(0);
     auto pT = track.GetPt(); // getting transverse momentum
@@ -55,7 +58,7 @@ void AnalysisTask::Exec() {
     auto rapidity = track.GetRapidity(); // getting rapidity
 	  auto p = track.GetP(); // getting absolute value of momentum
     auto phi = track.GetPhi(); // getting phi
-
+  
     auto beta = hit.GetField<float>(fields_id_.at(FIELDS::BETA)); // getting beta from meta_hits
     auto m2 = hit.GetField<float>(fields_id_.at(FIELDS::M2)); // getting mass squarred from meta_hits
     
@@ -81,8 +84,8 @@ void AnalysisTask::Exec() {
     PHIvPT_->Fill(phi, pT);
     PTvRAPIDITY_->Fill(rapidity, pT);
     PTvPSEUDORAPIDITY_->Fill(eta, pT);
-    MOMENTUMvBETA_->Fill(p*charge, beta); // *charge to see negatively charged
-    M2vMOMENTUM_->Fill(p, m2);
+    MOMENTUMvBETA_->Fill(p/charge, beta); // charge to see negatively charged
+    M2vMOMENTUM_->Fill(p/charge, m2);
     RAPIDITYvPHI_->Fill(phi, rapidity);
       }
     }
---
+++ src/analysis_task.h
@@ -0,0 +1,57 @@
+//
+// Created by mikhail on 6/16/20.
+//
+
+#ifndef QUALITY_ASSURANCE_SRC_TREE_READER_H_
+#define QUALITY_ASSURANCE_SRC_TREE_READER_H_
+
+#include <TChain.h>
+#include <TH3F.h>
+#include <TProfile2D.h>
+
+#include <AnalysisTree/EventHeader.hpp>
+#include <AnalysisTree/FillTask.hpp>
+#include <AnalysisTree/Cuts.hpp>
+#include <AnalysisTree/Detector.hpp>
+#include <AnalysisTree/Matching.hpp>
+
+namespace AnalysisTree {
+class AnalysisTask : public FillTask{
+public:
+ AnalysisTask() = default;
+  ~AnalysisTask() override = default;
+  void Init( std::map<std::string, void*>& branch_map ) override;
+  void Exec() override;
+  void Finish() override;
+private:
+ enum class FIELDS{ // enumerator to fast access to detectors' fields
+   HITS_TOF, 	// Hits in TOF-system
+   M2,		// Squarred mass derived from forward wall detectors (meta_hits branch)
+   BETA		// Beta derived from TOF-system (meta_hits branch)
+  };
+  std::map<FIELDS, int> fields_id_; // map to match detectors' fields with enumerator
+
+  /* pointers to link tree's branches with */
+  EventHeader* event_header_{nullptr}; 		// event info
+  Particles* mdc_vtx_tracks_{nullptr}; 		// tracks
+  HitDetector* meta_hits_{nullptr}; 		// TOF-system
+  Matching* mdc_meta_matching_{nullptr}; 	// matching between tracking system and TOF-system
+
+  TH1F* tof_multiplicity_distribution_; 	// 1D histogram of sum multiplicity in TOF system
+  TH1F* pT_distribution_; 			// 1D histogram of track transverse momentum distribution
+  TH1F* eta_distribution_;  			// 1D histogram of pseudorapidiity
+  TH1F* phi_distribution_;			// 1D histogram of phi angle
+  TH1F* beta_distribution_;			// 1D histogram of beta
+  TH1F* mass2_distribution_;			// 1D histogram of squarred mass
+  TH1F* p_distribution_;			// 1D histogram of absolute momentum
+  
+TH2F* PHIvPT_;					// 2D histogram of phi angle versus transverse kick
+TH2F* PTvRAPIDITY_;				// 2D histogram of transverse kick versus rapidity
+TH2F* PTvPSEUDORAPIDITY_;
+TH2F* MOMENTUMvBETA_;				// 2D histogram of absolute momentum versus beta
+TH2F* M2vMOMENTUM_;				// 2D histogram of squarred mass versus absolute momentum
+TH2F* RAPIDITYvPHI_;				// 2D histogram of rapidity versus phi angle
+
+};
+} // namespace AnalysisTree
+#endif // QUALITY_ASSURANCE_SRC_TREE_READER_H_
