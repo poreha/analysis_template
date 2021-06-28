d80a727725b9f887adf1e99bf9588a7e96fa82d6
Alex <pavlikbulgakov@gmail.com>
2021-06-28 15:55:07 +0300

    Redone binning
    gets you PID, PDG-reco, PDG etc (PDG->PDG-reco, GEN->PDG)

--- src/analysis_task.cc
+++ src/analysis_task.cc
@@ -39,7 +39,6 @@ namespace AnalysisTree
     fields_id_.insert(std::make_pair(FIELDS::TOF, meta_hits_config.GetFieldId("time_of_flight")));
     fields_id_.insert(std::make_pair(FIELDS::PATH_LEN, meta_hits_config.GetFieldId("path_length")));
 
-
     // initializing histograms
     tof_multiplicity_distribution_ = new TH1F("tof_multiplicity", ";TOF hits;counts", 100, 0, 100);
 
@@ -50,30 +49,52 @@ namespace AnalysisTree
     mass2_distribution_branch_ = new TH1F("mass_squarred_branch", "mass^2, [GeV]", 1024, -6, 6);
     mass_comparrison_ = new TH1F("mass_comparrison", "delta_mass^2, [what]", 1024, -6, 6);
     //p_distribution_ = new TH1F("p_distribution", "momentum, [GeV/c]", 1024, -50, 300);
+    impact_parameter_ = new TH1F("impact parameter", "?", 1024, 0, 9);
+    selected_tof_rpc_centrality_ = new TH1F("centrality_tofrpc", "?", 1024, 0, 10);
+
+    IMPvPT_ = new TH2F("Impact_parameter_vs_Pt","IMPvPt;impact parameter;p_{T}",512,0,9,250,0,2.);
 
     PTvRAPIDITY_ = new TH2F("Rapidity_vs_pT", ";Rapidity;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
     PTvPSEUDORAPIDITY_ = new TH2F("Pseudo-Rapidity_vs_pT", ";Pseudo-Rapidity;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
 
-    PID_RECO_ = new TH2F("PID_reco", "All reconstructed protons;Y;p_{T}", 250, 0., 2.5, 250, 0., 2.);
-    PID_PRIM_ = new TH2F("PID_prim", "Protons matched with MC-primary particle;Y;p_{T}", 250, 0., 2.5, 250, 0., 2.);
-    PID_SEC_ = new TH2F("PID_sec", "Protons matched with MC-secondary particle;Y;p_{T}", 250, 0., 2.5, 250, 0., 2.);
-    PDG_PRIM_ = new TH2F("PDG_prim", "Protons matched with MC-primary proton;Y;p_{T}", 250, 0., 2.5, 250, 0., 2.);
-    PDG_SEC_ = new TH2F("PDG_sec", "Protons matched with MC-secondary proton;Y;p_{T}", 250, 0., 2.5, 250, 0., 2.);
-    MISMATCH_ = new TH2F("Mismatch", "Protons matched with all MC-particles except protons;Y;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
-    LOSS_ = new TH2F("Loss", "All MC-particles, except protons, matched with MC-protons;Y;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
-    GEN_ = new TH2F("GEN", "All MC-protons;Y;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
-    GEN_PRIM_ = new TH2F("GEN_prim", "All primary MC-protons;Y;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
-    GEN_SEC_ = new TH2F("GEN_sec", "All secondary MC-protons;Y;p_{T}, GeV/c", 250, 0, 2.5, 250, 0, 2.);
+    // use legend
+    PID_RECO_ = new TH2F("PID_reco", "All reconstructed protons;Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+    PID_PRIM_ = new TH2F("PID_prim", "Reconstructed primary protons matched with MC-primary particles;Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+    PID_SEC_ = new TH2F("PID_sec", "Reconstructed secondary protons matched with MC-secondary particles;Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+
+    PID_PRIM_PDG_ = new TH2F("PID_prim_pdg", "Reconstructed primary protons matched with MC-primary particles (NO CUTS);Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+    PID_SEC_PDG_ = new TH2F("PID_sec_pdg", "Reconstructed secondary protons matched with MC-secondary particles (NO CUTS);Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+    PID_PRIM_CUTS_ = new TH2F("PID_prim_cuts", "Reconstructed primary protons matched with MC-primary particles (ONLY CUTS);Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+    PID_SEC_CUTS_ = new TH2F("PID_sec_cuts", "Reconstructed secondary protons matched with MC-secondary particles (ONLY CUTS);Y;p_{T}, [GeV/c]", 120, 0., 2.5, 120, 0., 2.5);
+
+    PDG_PRIM_ = new TH2F("PDG_prim_reco", "Particles matched with MC-primary proton;Y;p_{T}, [GeV/c]", 120, 0., 2.5, 250, 0., 2.5);
+    PDG_SEC_ = new TH2F("PDG_sec_reco", "Particles matched with MC-secondary proton;Y;p_{T}, [GeV/c]", 120, 0., 2.5, 250, 0., 2.5);
+
+    MISMATCH_ = new TH2F("Mismatch", "Protons matched with all MC-particles except protons;Y;p_{T}, [GeV/c]", 120, 0, 2.5, 120, 0, 2.5);
+    LOSS_ = new TH2F("Loss", "All MC-particles, except protons, matched with MC-protons;Y;p_{T}, [GeV/c]", 120, 0, 2.5, 120, 0, 2.5);
+
+    GEN_ = new TH2F("PDG", "All MC-protons;Y;p_{T}, [GeV/c]", 120, -1.5, 2.5, 120, 0, 2.5);
+    GEN_PRIM_ = new TH2F("PDG_prim", "All primary MC-protons;Y;p_{T}, [GeV/c]", 120, -1.5, 2.5, 120, 0., 2.5);
+    GEN_SEC_ = new TH2F("PDG_sec", "All secondary MC-protons;Y;p_{T}, [GeV/c]", 120, -1.5, 2.5, 120, 0, 2.5);
 
     PHIvPT_ = new TH2F("Phi_vs_pT", ";Phi;p_{T}, GeV", 256, -3.5, 3.5, 250, 0, 5);
     MOMENTUMvBETA_ = new TH2F("Momentum_vs_beta", ";p/q, GeV/c; beta", 256, -3, 6.2, 256, 0, 1.3);
     M2vMOMENTUM_ = new TH2F("Momentum_vs_Mass2", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60); // check it
     RAPIDITYvPHI_ = new TH2F("Rapidity_vs_Phi", ";Phi ;Rapidity", 256, -3.5, 3.5, 256, 0, 2);
 
-    M2vMOMENTUM_PID_PRIM = new TH2F("pid_prim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
-    M2vMOMENTUM_PID_SEC = new TH2F("pid_sec", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
-    M2vMOMENTUM_PDG_PRIM = new TH2F("pdg_prim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
-    M2vMOMENTUM_PDG_SEC = new TH2F("pdg_sec", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_PID_PRIM_ = new TH2F("pid_prim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_PID_SEC_ = new TH2F("pid_sec", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_PDG_PRIM_ = new TH2F("pdg_prim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_PDG_SEC_ = new TH2F("pdg_sec", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_MISMATCH_ = new TH2F("mismatch", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_MISMATCH_SIM_ = new TH2F("mismatch_sim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_LOSS_ = new TH2F("loss", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_LOSS_SIM_ = new TH2F("loss_sim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+
+    M2vP_GEN_ = new TH2F("gen", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_GEN_PRIM_ = new TH2F("gen_prim", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+    M2vP_GEN_SEC_ = new TH2F("gen_sec", ";p/q, GeV/c;mass^2, GeV", 256, -6, 6, 512, -10, 60);
+
     PvTOF_ = new TH2F("Momentum_vs_TimeOfFlight", ";p/q, GeV/c; t", 256, -6, 6, 512, 0, 100);
     PvPathLen_ = new TH2F("Momentum_vs_PathLength", ";p/q, GeV/c; path", 256, -6, 6, 512, 1000, 3000);
   }
@@ -83,6 +104,8 @@ namespace AnalysisTree
     int proton = 2212; //int particle ID
     //auto hits_tof = event_header_->GetField<int>(fields_id_.at(FIELDS::HITS_TOF)); // getting multiplicity from event header
     //tof_multiplicity_distribution_->Fill(hits_tof); // filling histogram
+
+//auto im_param = sim_header_->GetField<float>(1); // impact parameter // посмотреть по событиям, а не по трекам
 
     int n_tracks = mdc_vtx_tracks_->GetNumberOfChannels(); // number of tracks in current event
     for (int i = 0; i < n_tracks; ++i)
@@ -108,6 +131,9 @@ namespace AnalysisTree
       auto eta = track.GetEta();                   // getting pseudorapidity
       auto rapidity = track.GetRapidity();         // getting rapidity
       auto sim_rapidity = sim_track.GetRapidity(); // getting rapidity of matched simulated track
+
+      auto sim_p = sim_track.GetP();
+      auto sim_m2 = sim_track.GetMass();
 
       auto p = track.GetP(); // getting absolute value of momentum
 
@@ -149,70 +175,75 @@ namespace AnalysisTree
       bool is_prim = sim_track.GetField<bool>(0); // sim_particle is a primary
 
       int particle = proton; // proton
-
+      auto selected_centrality = event_header_->GetField<float>(0);
+      selected_tof_rpc_centrality_->Fill(selected_centrality);
       // Filling histograms
       //mass2_distribution_branch_->Fill(m2);
+      if (kNoVeto && kGoodVertexCand && kGoodStartMeta){
       if (pid == particle)
         M2vMOMENTUM_->Fill(p, m2);
 
       if (pid == particle)
-      { // reconstructed = certain particles
-        PID_RECO_->Fill(rapidity, pT);
+      {
+        PID_RECO_->Fill(rapidity, pT); // all reconstructed certain particles
         // PID
-        if (is_prim && (dca_xy < 15. && dca_xy > -15. && dca_z < 15. && chi2 < 100.))
-        {                                // primary and cut applied
-          PID_PRIM_->Fill(rapidity, pT); // any matched particle is primary
-          M2vMOMENTUM_PID_PRIM->Fill(p, m2);
+        if (dca_xy < 15. && dca_xy > -15. && dca_z < 15. && chi2 < 100.)
+        {
+          PID_PRIM_CUTS_->Fill(rapidity, pT);
+          // if passes the cut as primary
+          if (is_prim)
+            {                                
+              PID_PRIM_->Fill(rapidity, pT); // any matched certain particle is primary
+              M2vP_PID_PRIM_->Fill(p, m2);
+            }
         }
         else
         {
-          PID_SEC_->Fill(rapidity, pT); // any matched particle is secondary
-          M2vMOMENTUM_PID_SEC->Fill(p, m2);
+          PID_SEC_CUTS_->Fill(rapidity, pT);
+          // if didn't pass the cut
+          if (!is_prim)
+          {
+            PID_SEC_->Fill(rapidity, pT); // any matched certain particle is secondary
+            M2vP_PID_SEC_->Fill(p, m2);
+          }
         }
 
-        
+        if (is_prim) PID_PRIM_PDG_->Fill(rapidity, pT);
+        else PID_SEC_PDG_->Fill(rapidity, pT);
       }
       // MISMATCH if pid == particle , sim_pid != particle
       else
       {
-        MISMATCH_->Fill(rapidity, pT); // reconstructed != certain particles
+        MISMATCH_->Fill(rapidity, pT);
+        M2vP_MISMATCH_->Fill(p, m2);
+        M2vP_MISMATCH_SIM_->Fill(sim_p, sim_m2);
       }
       //PTvRAPIDITY_->Fill(rapidity, pT); // PTvRAPIDITY == PID_RECO
       PTvPSEUDORAPIDITY_->Fill(eta, pT);
-    
-    // LOSS if pid != particle , sim_pid == particle
-    if (pid != particle)
-    {
-      if (sim_pid == particle)
+
+      // LOSS
+      if (pid != particle && sim_pid == particle)
       {
         LOSS_->Fill(rapidity, pT);
+        M2vP_LOSS_->Fill(p, m2);
+        M2vP_LOSS_SIM_->Fill(sim_p, sim_m2);
       }
-    }
-
-    // PDG
-    if (sim_pid == particle)
-    { // simulated = certain particles
-      if (is_prim)
-      {
-        PDG_PRIM_->Fill(rapidity, pT); // matched particle is primary
-        M2vMOMENTUM_PDG_PRIM->Fill(p, m2);
       }
-      else
-      {
-        PDG_SEC_->Fill(rapidity, pT); // matched particle is secondary
-        M2vMOMENTUM_PDG_SEC->Fill(p, m2);
-      }
-    }
-/*
+      // PDG
       if (sim_pid == particle)
       {
-        GEN_->Fill(sim_rapidity, sim_pT);
         if (is_prim)
-          GEN_PRIM_->Fill(sim_rapidity, sim_pT);
+        {
+          PDG_PRIM_->Fill(sim_rapidity, sim_pT); // matched particle is primary // changed to sim_
+          M2vP_PDG_PRIM_->Fill(p, m2);
+        }
         else
-          GEN_SEC_->Fill(sim_rapidity, sim_pT);
+        {
+          PDG_SEC_->Fill(sim_rapidity, sim_pT); // matched particle is secondary
+          M2vP_PDG_SEC_->Fill(p, m2);
+        }
       }
-*/
+
       //PHIvPT_->Fill(phi, pT);
       //MOMENTUMvBETA_->Fill(p, beta); // charge to see negatively charged
       //M2vMOMENTUM_->Fill(p, m2);
@@ -221,31 +252,52 @@ namespace AnalysisTree
       beta_distribution_->Fill(beta);
     }
 
+    // GEN
     int sim_ntracks = sim_tracks_->GetNumberOfChannels();
-    for(int sim_i = 0; sim_i < sim_ntracks; sim_i++)
+    for (int sim_i = 0; sim_i < sim_ntracks; sim_i++)
     {
-      auto sim_track = sim_tracks_->GetChannel(sim_i);    // getting matched simulated track // changed i->matched_track
-      int sim_pid = sim_track.GetPid();                           // getting PID of matched simulated track
+      auto sim_track = sim_tracks_->GetChannel(sim_i); // getting matched simulated track // changed i->matched_track
+      int sim_pid = sim_track.GetPid();                // getting PID of matched simulated track
 
-      auto sim_pT = sim_track.GetPt(); // getting transverse momentum of matched simulated track
+      auto sim_pT = sim_track.GetPt();             // getting transverse momentum of matched simulated track
       auto sim_rapidity = sim_track.GetRapidity(); // getting rapidity of matched simulated track
+
+      auto sim_p = sim_track.GetP();
+      auto sim_m2 = sim_track.GetMass();
 
       bool is_prim = sim_track.GetField<bool>(0); // sim_particle is a primary
 
       if (sim_pid == 2212)
       {
+
         GEN_->Fill(sim_rapidity, sim_pT);
+        M2vP_GEN_->Fill(sim_p, sim_m2);
         if (is_prim)
+        {
           GEN_PRIM_->Fill(sim_rapidity, sim_pT);
+          M2vP_GEN_PRIM_->Fill(sim_p, sim_m2);
+        }
         else
+        {
           GEN_SEC_->Fill(sim_rapidity, sim_pT);
+          M2vP_GEN_SEC_->Fill(sim_p, sim_m2);
+        }
       }
     }
+    
   }
 
   void AnalysisTask::Finish()
   {
+    GEN_PRIM_->Sumw2();
+    PDG_PRIM_->Sumw2();
     // Writing histograms to file
+    selected_tof_rpc_centrality_->Write();
+    
+    PID_PRIM_PDG_->Write();
+    PID_SEC_PDG_->Write();
+    PID_PRIM_CUTS_->Write();
+    PID_SEC_CUTS_->Write();
 
     PID_RECO_->Write();
     PID_PRIM_->Write();
@@ -260,10 +312,18 @@ namespace AnalysisTree
 
     //PTvRAPIDITY_->Write();
     PTvPSEUDORAPIDITY_->Write();
-    M2vMOMENTUM_PID_PRIM->Write();
-    M2vMOMENTUM_PID_SEC->Write();
-    M2vMOMENTUM_PDG_PRIM->Write();
-    M2vMOMENTUM_PDG_SEC->Write();
+    M2vP_PID_PRIM_->Write();
+    M2vP_PID_SEC_->Write();
+    M2vP_PDG_PRIM_->Write();
+    M2vP_PDG_SEC_->Write();
+    M2vP_MISMATCH_->Write();
+    M2vP_MISMATCH_SIM_->Write();
+    M2vP_LOSS_->Write();
+    M2vP_LOSS_SIM_->Write();
+
+    M2vP_GEN_->Write();
+    M2vP_GEN_PRIM_->Write();
+    M2vP_GEN_SEC_->Write();
 
     //mass2_distribution_->Write();
     //mass2_distribution_branch_->Write();
--- src/analysis_task.h
+++ src/analysis_task.h
@@ -15,82 +15,100 @@
 #include <AnalysisTree/Detector.hpp>
 #include <AnalysisTree/Matching.hpp>
 
-namespace AnalysisTree {
-class AnalysisTask : public FillTask{
-public:
- AnalysisTask() = default;
-  ~AnalysisTask() override = default;
-  void Init( std::map<std::string, void*>& branch_map ) override;
-  void Exec() override;
-  void Finish() override;
-private:
- enum class FIELDS{ // enumerator to fast access to detectors' fields
-   HITS_TOF, 	// Hits in TOF-system
-   M2,		// Squarred mass derived from forward wall detectors (meta_hits branch)
-   BETA,	// Beta derived from TOF-system (meta_hits branch)
-   CHARGE,	// Charge from curve (meta_hits branch)
-   PT2,		// Physical Trigger 2 (event_header branch)
-   VERT_CAN,
-   TOF,		// Time of Flight (meta_hits branch)
-   PATH_LEN
+namespace AnalysisTree
+{
+  class AnalysisTask : public FillTask
+  {
+  public:
+    AnalysisTask() = default;
+    ~AnalysisTask() override = default;
+    void Init(std::map<std::string, void *> &branch_map) override;
+    void Exec() override;
+    void Finish() override;
+
+  private:
+    enum class FIELDS
+    {           // enumerator to fast access to detectors' fields
+      HITS_TOF, // Hits in TOF-system
+      M2,       // Squarred mass derived from forward wall detectors (meta_hits branch)
+      BETA,     // Beta derived from TOF-system (meta_hits branch)
+      CHARGE,   // Charge from curve (meta_hits branch)
+      PT2,      // Physical Trigger 2 (event_header branch)
+      VERT_CAN,
+      TOF, // Time of Flight (meta_hits branch)
+      PATH_LEN
+    };
+    enum class uPARTICLES
+    {
+      PROTON,
+      KAON
+    };
+
+    std::map<FIELDS, int> fields_id_; // map to match detectors' fields with enumerator
+    std::map<uPARTICLES, int> particle_id_;
+
+    /* pointers to link tree's branches with */
+    EventHeader *event_header_{nullptr};   // event info
+    Particles *mdc_vtx_tracks_{nullptr};   // tracks
+    HitDetector *meta_hits_{nullptr};      // TOF-system
+    Matching *mdc_meta_matching_{nullptr}; // matching between tracking system and TOF-system
+
+    EventHeader *sim_header_{nullptr}; // event info for simulated
+    Particles *sim_tracks_{nullptr};   // tracks for simulated
+    Matching *track_matching_{nullptr};
+
+    TH1F *tof_multiplicity_distribution_; // 1D histogram of sum multiplicity in TOF system
+    TH1F *pT_distribution_;               // 1D histogram of track transverse momentum distribution
+    TH1F *eta_distribution_;              // 1D histogram of pseudorapidiity
+    TH1F *phi_distribution_;              // 1D histogram of phi angle
+    TH1F *beta_distribution_;             // 1D histogram of beta
+    TH1F *mass2_distribution_;            // 1D histogram of squarred mass
+    TH1F *p_distribution_;                // 1D histogram of absolute momentum
+    TH1F *mass2_distribution_branch_;     // 1D histogram of squarred mass from branch
+    TH1F *mass_comparrison_;
+    TH1F *impact_parameter_;
+    TH1F *selected_tof_rpc_centrality_;
+
+    TH2F *PHIvPT_;      // 2D histogram of phi angle versus transverse kick
+    TH2F *PTvRAPIDITY_; // 2D histogram of transverse kick versus rapidity
+    TH2F *PTvPSEUDORAPIDITY_;
+    TH2F *MOMENTUMvBETA_; // 2D histogram of absolute momentum versus beta
+    TH2F *M2vMOMENTUM_;   // 2D histogram of squarred mass versus absolute momentum
+    TH2F *RAPIDITYvPHI_;  // 2D histogram of rapidity versus phi angle
+
+    TH2F *M2vP_PID_PRIM_;
+    TH2F *M2vP_PID_SEC_;
+    TH2F *M2vP_PDG_PRIM_;
+    TH2F *M2vP_PDG_SEC_;
+    TH2F *M2vP_MISMATCH_;
+    TH2F *M2vP_MISMATCH_SIM_;
+    TH2F *M2vP_LOSS_;
+    TH2F *M2vP_LOSS_SIM_;
+
+    TH2F *M2vP_GEN_;
+    TH2F *M2vP_GEN_PRIM_;
+    TH2F *M2vP_GEN_SEC_;
+
+    TH2F *PID_RECO_;
+    TH2F *PID_PRIM_;
+    TH2F *PID_SEC_;
+    TH2F *PID_PRIM_PDG_;
+    TH2F *PID_SEC_PDG_;
+    TH2F *PID_PRIM_CUTS_;
+    TH2F *PID_SEC_CUTS_;
+    TH2F *PDG_PRIM_;
+    TH2F *PDG_SEC_;
+    TH2F *MISMATCH_;
+    TH2F *LOSS_;
+    TH2F *GEN_;
+    TH2F *GEN_PRIM_;
+    TH2F *GEN_SEC_;
+
+    TH2F *IMPvPT_;
+    TH2F *Efficiency_Acceptance_;
+
+    TH2F *PvTOF_;
+    TH2F *PvPathLen_;
   };
-  enum class uPARTICLES{
-    PROTON,
-    KAON
-  };
-  
-  std::map<FIELDS, int> fields_id_; // map to match detectors' fields with enumerator
-  std::map<uPARTICLES, int> particle_id_;
-
-  /* pointers to link tree's branches with */
-  EventHeader* event_header_{nullptr}; 		// event info
-  Particles* mdc_vtx_tracks_{nullptr}; 		// tracks
-  HitDetector* meta_hits_{nullptr}; 		// TOF-system
-  Matching* mdc_meta_matching_{nullptr}; 	// matching between tracking system and TOF-system
-  
-  EventHeader* sim_header_{nullptr}; 		// event info for simulated
-  Particles* sim_tracks_{nullptr}; 		// tracks for simulated
-  Matching* track_matching_{nullptr};
-
-  TH1F* tof_multiplicity_distribution_; 	// 1D histogram of sum multiplicity in TOF system
-  TH1F* pT_distribution_; 			// 1D histogram of track transverse momentum distribution
-  TH1F* eta_distribution_;  			// 1D histogram of pseudorapidiity
-  TH1F* phi_distribution_;			// 1D histogram of phi angle
-  TH1F* beta_distribution_;			// 1D histogram of beta
-  TH1F* mass2_distribution_;			// 1D histogram of squarred mass
-  TH1F* p_distribution_;			// 1D histogram of absolute momentum
-  TH1F* mass2_distribution_branch_;			// 1D histogram of squarred mass from branch
-  TH1F* mass_comparrison_;
-
-
-  TH2F* PHIvPT_;					// 2D histogram of phi angle versus transverse kick
-  TH2F* PTvRAPIDITY_;				// 2D histogram of transverse kick versus rapidity
-  TH2F* PTvPSEUDORAPIDITY_;
-  TH2F* MOMENTUMvBETA_;				// 2D histogram of absolute momentum versus beta
-  TH2F* M2vMOMENTUM_;				// 2D histogram of squarred mass versus absolute momentum
-  TH2F* RAPIDITYvPHI_;				// 2D histogram of rapidity versus phi angle
-
-  TH2F* M2vMOMENTUM_PID_PRIM;
-  TH2F* M2vMOMENTUM_PID_SEC;
-  TH2F* M2vMOMENTUM_PDG_PRIM;
-  TH2F* M2vMOMENTUM_PDG_SEC;
-
-  TH2F* PID_RECO_;
-  TH2F* PID_PRIM_;
-  TH2F* PID_SEC_;
-  TH2F* PDG_PRIM_;
-  TH2F* PDG_SEC_;
-  TH2F* MISMATCH_;
-  TH2F* LOSS_;
-  TH2F* GEN_;
-  TH2F* GEN_PRIM_;
-  TH2F* GEN_SEC_;
-
-  TH2F* PvTOF_;
-  TH2F* PvPathLen_;
-
-
-
-};
 } // namespace AnalysisTree
 #endif // QUALITY_ASSURANCE_SRC_TREE_READER_H_
